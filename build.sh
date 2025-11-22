a6da11ac6001fe3a4a670690a2edf14de004a64a
Ultimate88 <alejandrogonzalez88x@gmail.com>
2025-11-22 13:12:49 +0100

    fix: :bug: Fix build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,7 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-
-# Convert static asset files
+cd charmaway/
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
 python manage.py migrate
