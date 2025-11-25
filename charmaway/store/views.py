8b613d8fbf1da98b8b9b942f3ed746b1c283e452
josegicar <josegicar@alum.us.es>
2025-11-25 23:22:18 +0100

    fix: 🐛 Delete Offers and New Products Sections

--- charmaway/store/views.py
+++ charmaway/store/views.py
@@ -13,24 +13,9 @@ def home(request):
     # Get all departments
     departments = Department.objects.all().order_by('order_position', 'name')
 
-    # Get products with offers
-    offer_products = Product.objects.filter(
-        is_available=True,
-        offer_price__isnull=False
-    ).select_related('brand', 'category').prefetch_related('images')[:4]
-
-    # Get newest products
-    newest_products = Product.objects.exclude(
-        category__department__name='Servicios'
-    ).filter(
-        is_available=True
-    ).select_related('brand', 'category').prefetch_related('images').order_by('-created_at')[:4]
-
     context = {
         'featured_products': featured_products,
         'departments': departments,
-        'offer_products': offer_products,
-        'newest_products': newest_products,
     }
 
     return render(request, 'store/home.html', context)
